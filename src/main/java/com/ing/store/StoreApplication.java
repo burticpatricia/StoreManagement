dcda97784d2ae3e1a98c2457eb5988ae675ed896
Patricia Burtic <patricia.burtic@gmail.com>
2024-05-03 22:47:07 +0300

    Create ProductController mock impl

--- src/main/java/com/ing/store/StoreApplication.java
+++ src/main/java/com/ing/store/StoreApplication.java
@@ -2,8 +2,10 @@ package com.ing.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"org.openapitools.api", "com.ing.store.controller"})
 public class StoreApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ing/store/controller/ProductController.java
+++ src/main/java/com/ing/store/controller/ProductController.java
@@ -1,2 +1,14 @@
-package com.ing.store.controller;public class ProductController {
+package com.ing.store.controller;
+
+import org.openapitools.api.ProductsApiDelegate;
+import org.openapitools.model.ProductDto;
+import org.openapitools.model.ProductTypeDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductController implements ProductsApiDelegate {
+    @Override
+    public ProductDto getProductById(Long productId) {
+        return new ProductDto(2, "hello", 3.3d, ProductTypeDto.FOOD, 45);
+    }
 }
