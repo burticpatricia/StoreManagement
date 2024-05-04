66954ccc00248149dbdbd4dd0903e2faeb30cc53
Patricia Burtic <patricia.burtic@gmail.com>
2024-05-04 12:41:59 +0300

    Fix mapping of type in Mapper intterface

--- src/main/java/com/ing/store/api/mapper/ProductMapper.java
+++ src/main/java/com/ing/store/api/mapper/ProductMapper.java
@@ -2,6 +2,7 @@ package com.ing.store.api.mapper;
 
 import com.ing.store.domain.entity.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import org.openapitools.model.ProductDto;
 
@@ -12,7 +13,9 @@ import java.util.List;
 public interface ProductMapper {
     ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
 
+    @Mapping(target = "productType", source = "product.type")
     ProductDto dtoFromProduct(@NotNull Product product);
+    @Mapping(target = "type", source = "dto.productType")
     Product productFromDto(@NotNull ProductDto dto);
     List<ProductDto> dtoListFromProductList(List<Product> productList);
     List<Product> listFromProductDtoList(List<ProductDto> dtoList);
